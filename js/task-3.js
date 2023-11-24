1c933e6563a83be6bbf653a503d1d0abd79416d3
Evgeniy-Popov-98 <evgeniy6183@gmail.com>
2023-11-24 22:30:22 +0200

    HomeWork#3

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,17 @@
 'use strict';
 
-function getElementWidth(content, padding, border) {
-  content = Number.parseInt(content);
-  padding = Number.parseInt(padding) * 2;
-  border = Number.parseFloat(border) * 2;
-
-  const widht = content + padding + border;
-
-  return widht;
+function filterArray(numbers, value) {
+  let newNumberArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      newNumberArray.push(numbers[i]);
+    }
+  }
+  return newNumberArray;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
